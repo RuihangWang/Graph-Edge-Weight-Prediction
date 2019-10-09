8200a79dcb909c56fa45d14820bedba739f64f66
guludeemo <shenmeng1997@gmail.com>
2019-10-09 15:26:40 +0800

    update page_rank

--- page_rank.py
+++ page_rank.py
@@ -1,22 +1,42 @@
 """
 Page Rank
 """
-
+import networkx as nx
 def pagerank_predict_weight(G, G_n, PR):
     RMSE = 0
     iter = 0
-    for (u, v, w) in G.edges(data='signed_weight'):
+    target = len(G.edges()) - len(G_n.edges())
+    G_PR = G_n.copy()
+
+    w_in = {}
+    w_out ={}
+    PR_total_out = {}
+    PR_total_in = {}
+    for (u,w) in G_PR.nodes(data='weight'):
+        w_in_v = 0
+        w_out_v = 0
+        PR_total_out_v = 0
+        PR_total_in_v = 0
+        for edge in G_n.out_edges(u, data='weight'):
+            w_out_v += edge[2] * PR[edge[1]]
+            PR_total_out_v += PR[edge[1]]
+        for edge in G_n.in_edges(u,data='weight'):
+            w_in_v += edge[2] * PR[edge[0]]
+            PR_total_in_v += PR[edge[0]]
+        w_in[u] = w_in_v
+        w_out[u] = w_out_v
+        PR_total_in[u] = PR_total_in_v
+        PR_total_out[u] = PR_total_out_v
+    nx.set_node_attributes(G_PR, w_in, 'w_in')
+    nx.set_node_attributes(G_PR, w_out, 'w_out')
+    nx.set_node_attributes(G_PR, PR_total_in, 'PR_total_in')
+    nx.set_node_attributes(G_PR, PR_total_out, 'PR_total_out')
+
+    for (u, v, w) in G.edges(data='weight'):
         if G_n.has_edge(u, v):
             continue
-
-        w_ = 0
-        PR_total = 0
-        for edge in G_n.out_edges(u,data='signed_weight'):
-            w_ += edge[2] * PR[edge[1]]
-            PR_total += PR[edge[1]]
-        for edge in G_n.in_edges(v,data='signed_weight'):
-            w_ += edge[2] * PR[edge[0]]
-            PR_total += PR[edge[0]]
+        w_ = G_PR.node[u]['w_out'] + G_PR.node[v]['w_in']
+        PR_total = G_PR.node[u]['PR_total_out'] + G_PR.node[v]['PR_total_in']
         if PR_total != 0 :
             w_ /= PR_total
 
